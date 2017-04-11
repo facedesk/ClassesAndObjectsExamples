cf85edc0217d87021cd56e920a5b7238cfb81ae0
MrGold <dgold@sjnma.org>
2017-04-11 11:01:16 -0500

    ranking of cards fixed

--- card.py
+++ card.py
@@ -1,7 +1,7 @@
 class Card:
-    def __init__(self, rank, suit):
-    	self.rank = rank.lower()
-    	self.suit = suit.lower()
+    def __init__(self, passedInRank, passedInSuit):
+    	self.rank = passedInRank.lower()
+    	self.suit = passedInSuit.lower()
         self.orderRank = self.checkRank()
         self.orderSuit = self.checkSuit()
         if(self.orderRank!=0 and self.orderSuit!=0):
@@ -12,7 +12,34 @@ class Card:
         	self.suit = "Invalid"
 
     def checkRank(self):
-        return "invalid"
+        if(self.rank=="2"):
+            return 1
+        elif(self.rank=="3"):
+            return 2
+        elif(self.rank=="4"):
+            return 3
+        elif(self.rank=="5"):
+            return 4 
+        elif(self.rank=="6"):
+            return 5  
+        elif(self.rank=="7"):
+            return 6  
+        elif(self.rank=="8"):
+            return 7  
+        elif(self.rank=="9"):
+            return 8  
+        elif(self.rank=="10"):
+            return 9 
+        elif(self.rank=="jack"):
+            return 10
+        elif(self.rank=="queen"):
+            return 11     
+        elif(self.rank=="king"):
+            return 12
+        elif(self.rank=="ace"):
+            return 13
+        else:
+            return 0
 
     def checkSuit(self):
     	if(self.suit == "clubs"):
@@ -31,5 +58,12 @@ class Card:
 
 
 if(__name__=="__main__"):
-    c = Card("Ace","Clubs")
+    a = Card("Ace","Clubs")
+    b = Card("5","DiaMOnds")
+    c = Card("4","hearts")  
+    d= Card("Blue", "5")
+
+    a.displayCard()
+    b.displayCard()
     c.displayCard()
+    d.displayCard()
